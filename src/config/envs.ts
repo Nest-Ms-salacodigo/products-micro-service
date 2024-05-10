e846448167eae287388249d9458122ba5245705a
Santiago Salacodigo <santiago.salamanca7@gmail.com>
2024-05-10 17:13:42 -0500

    First commit

---
+++ src/config/envs.ts
@@ -0,0 +1,35 @@
+import 'dotenv/config';
+import * as joi from 'joi';
+
+
+
+interface EnvVars {
+    PORT: number;
+    DATABASE_URL: string;
+}
+
+
+// Validador de esquema
+const envSchema = joi.object({
+    PORT: joi.number().required(),
+    DATABASE_URL: joi.string().required(),
+})
+.unknown(true) // Para aceptar todo lo que no estemos validando
+
+
+const { error, value } = envSchema.validate( process.env );
+
+
+if( error ){
+    throw new Error(`Config validation error: ${ error.message }`);
+}
+
+const envVars: EnvVars = value;
+
+
+
+export const envs = {
+    port: envVars.PORT,
+    databaseUrl: envVars.DATABASE_URL,
+    
+}
